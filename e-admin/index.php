5f0418c3ced1b8510af2af130329e48fb5388364
Atiq Ramzan <prenerosolutions@gmail.com>
2023-05-15 10:35:16 +0500

    icon

---
+++ e-admin/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="zxx">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />	
+		
+		<title>admin | Efone co uk</title>
+	
+		<link rel="icon" href="../img/icon.png" type="image/png">
+		<link rel="stylesheet" href="css/bootstrap1.min.css" />
+		<link rel="stylesheet" href="vendors/themefy_icon/themify-icons.css" />
+		<link rel="stylesheet" href="vendors/niceselect/css/nice-select.css" />
+		<link rel="stylesheet" href="vendors/owl_carousel/css/owl.carousel.css" />
+		<link rel="stylesheet" href="vendors/gijgo/gijgo.min.css" />
+		<link rel="stylesheet" href="vendors/font_awesome/css/all.min.css" />
+		<link rel="stylesheet" href="vendors/tagsinput/tagsinput.css" />
+		<link rel="stylesheet" href="vendors/datepicker/date-picker.css" />
+		<link rel="stylesheet" href="vendors/scroll/scrollable.css" />
+		<link rel="stylesheet" href="vendors/datatable/css/jquery.dataTables.min.css" />
+		<link rel="stylesheet" href="vendors/datatable/css/responsive.dataTables.min.css" />
+		<link rel="stylesheet" href="vendors/datatable/css/buttons.dataTables.min.css" />
+		<link rel="stylesheet" href="vendors/text_editor/summernote-bs4.css" />
+		<link rel="stylesheet" href="vendors/morris/morris.css">
+		<link rel="stylesheet" href="vendors/material_icon/material-icons.css" />
+		<link rel="stylesheet" href="css/metisMenu.css">
+		<link rel="stylesheet" href="css/style1.css" />
+		<link rel="stylesheet" href="css/colors/default.css" id="colorSkinCSS">
+
+	</head>
+
+	
+	<body class="crm_body_bg">
+		<div class="main_content_iner ">
+			<div class="container-fluid p-0">
+				<div class="row justify-content-center">
+					<img src="../img/logo.png" alt="" style="width: 30%; margin-top: 50px;">					
+					<div class="col-lg-12">
+						<div class="white_box mb_30">
+							<div class="row justify-content-center">
+								<div class="col-lg-6">
+									<div class="modal-content cs_modal">
+										<div class="modal-header justify-content-center theme_bg_1">
+											<h5 class="modal-title text_white">Log in</h5>
+										</div>
+										<div class="modal-body">
+											<form>
+												<div class="">
+													<input type="text" class="form-control" placeholder="Enter your email">
+												</div>
+												<div class="">
+													<input type="password" class="form-control" placeholder="Password">
+												</div>
+												<button type="submit" class="btn_1 full_width text-center">Log in</button>
+												
+												<div class="text-center">
+													<a href="#" data-bs-toggle="modal" data-bs-target="#forgot_password" data-bs-dismiss="modal" class="pass_forget_btn">
+														Forget Password?
+													</a>
+												</div>
+											</form>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="footer_part">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="footer_iner text-center">
+							<p>2023 © Efone Co UK - Designed by <a href="#"> <i class="ti-heart"></i>  </a><a href="https://www.prenero.com/"> Prenero Solutions</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+		<script src="js/jquery1-3.4.1.min.js"></script>
+		<script src="js/popper1.min.js"></script>
+		<script src="js/bootstrap1.min.js"></script>
+		<script src="js/metisMenu.js"></script>
+		<script src="vendors/count_up/jquery.waypoints.min.js"></script>
+		<script src="vendors/chartlist/Chart.min.js"></script>
+		<script src="vendors/count_up/jquery.counterup.min.js"></script>
+		<script src="vendors/niceselect/js/jquery.nice-select.min.js"></script>
+		<script src="vendors/owl_carousel/js/owl.carousel.min.js"></script>
+		<script src="vendors/datatable/js/jquery.dataTables.min.js"></script>
+		<script src="vendors/datatable/js/dataTables.responsive.min.js"></script>
+		<script src="vendors/datatable/js/dataTables.buttons.min.js"></script>
+		<script src="vendors/datatable/js/buttons.flash.min.js"></script>
+		<script src="vendors/datatable/js/jszip.min.js"></script>
+		<script src="vendors/datatable/js/pdfmake.min.js"></script>
+		<script src="vendors/datatable/js/vfs_fonts.js"></script>
+		<script src="vendors/datatable/js/buttons.html5.min.js"></script>
+		<script src="vendors/datatable/js/buttons.print.min.js"></script>
+		<script src="js/chart.min.js"></script>
+		<script src="vendors/progressbar/jquery.barfiller.js"></script>
+		<script src="vendors/tagsinput/tagsinput.js"></script>
+		<script src="vendors/text_editor/summernote-bs4.js"></script>
+		<script src="vendors/am_chart/amcharts.js"></script>
+		<script src="vendors/scroll/perfect-scrollbar.min.js"></script>
+		<script src="vendors/scroll/scrollable-custom.js"></script>
+		<script src="vendors/chart_am/core.js"></script>
+		<script src="vendors/chart_am/charts.js"></script>
+		<script src="vendors/chart_am/animated.js"></script>
+		<script src="vendors/chart_am/kelly.js"></script>
+		<script src="vendors/chart_am/chart-custom.js"></script>
+		<script src="js/custom.js"></script>
+	</body>
+</html>
